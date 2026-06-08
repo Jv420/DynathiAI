27ce599b9a45c915d4c6c0df4fb68cf0e900975a
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-06-09 00:55:34 +0200

    Add core module loader

---
+++ src/core/moduleLoader.js
@@ -0,0 +1,37 @@
+const mining = require("../modules/mining");
+const woodcutting = require("../modules/woodcutting");
+const fishing = require("../modules/fishing");
+const storage = require("../modules/storage");
+const farming = require("../modules/farming");
+const combat = require("../modules/combat");
+const survival = require("../modules/survival");
+const crafting = require("../modules/crafting");
+const building = require("../modules/building");
+const itemframes = require("../modules/itemframes");
+const sleep = require("../modules/sleep");
+const aiBrain = require("../modules/aiBrain");
+const animals = require("../modules/animals");
+const economy = require("../modules/economy");
+const navigation = require("../modules/navigation");
+
+function loadModules() {
+  return {
+    mining,
+    woodcutting,
+    fishing,
+    storage,
+    farming,
+    combat,
+    survival,
+    crafting,
+    building,
+    itemframes,
+    sleep,
+    aiBrain,
+    animals,
+    economy,
+    navigation
+  };
+}
+
+module.exports = { loadModules };
