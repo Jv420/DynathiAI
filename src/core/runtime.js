5133459d456536816cc7fbf2c4cfc18b6facb2da
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-06-09 00:56:24 +0200

    Add core runtime module

---
+++ src/core/runtime.js
@@ -0,0 +1,61 @@
+const { createLogger } = require("../utils/logger");
+const { loadModules } = require("./moduleLoader");
+const { createJobManager } = require("../modules/jobs");
+const { createBrainLoop } = require("../modules/aiBrain");
+
+function createRuntime() {
+  const logger = createLogger();
+  const modules = loadModules();
+
+  const state = {
+    bot: null,
+    mcData: null,
+    reconnecting: false,
+    discordClient: null
+  };
+
+  function getBot() {
+    return state.bot;
+  }
+
+  function getMcData() {
+    return state.mcData;
+  }
+
+  const jobManager = createJobManager({
+    bot: getBot,
+    mcData: getMcData,
+    actions: {
+      chopWood: modules.woodcutting.chopWood,
+      mineBlock: modules.mining.mineBlock,
+      fishOnce: modules.fishing.fishOnce,
+      farm: modules.farming.farm,
+      autoSell: modules.economy.sellInventory
+    },
+    log: logger.log
+  });
+
+  const brain = createBrainLoop(getBot, modules, Number(process.env.BRAIN_INTERVAL_MS) || 10000);
+
+  function setBot(bot) {
+    state.bot = bot;
+  }
+
+  function setMcData(mcData) {
+    state.mcData = mcData;
+  }
+
+  return {
+    state,
+    modules,
+    logger,
+    jobManager,
+    brain,
+    getBot,
+    getMcData,
+    setBot,
+    setMcData
+  };
+}
+
+module.exports = { createRuntime };
